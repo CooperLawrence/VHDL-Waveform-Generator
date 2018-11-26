aecec2afc7265534347ad8f7b3b7fd97f3f4f277
ledde008 <45053664+ledde008@users.noreply.github.com>
2018-11-26 16:12:39 -0500

    fixed some syntax errors

--- src/FSM.vhd
+++ src/FSM.vhd
@@ -4,7 +4,7 @@ use ieee.std_logic_1164.all;
 entity FSM is
 	port (
 		selection: in std_logic_vector(1 downto 0);
-		operation: out std_logic_vector (1 downto 0);
+		operation: out std_logic_vector (1 downto 0)
 	);
 end FSM;
 
@@ -14,37 +14,37 @@ architecture FSM_arch of FSM is
 	signal sel: std_logic_vector(1 downto 0);
 begin
 CS <= Sine;	
-process (CS) 
+process (CS, sel) 
 begin
 	case CS is
 		when Sine =>
-			if sel = '00' then NS <= Sine;
-			elsif sel = '01' then NS <= Triangle;
-			elsif sel= '10' then NS <= Square;
+			if sel = "00" then NS <= Sine;
+			elsif sel = "01" then NS <= Triangle;
+			elsif sel= "10" then NS <= Square;
 			else NS <= Special;
 			end if;
-			operation <= '00';
+			operation <= "00";
 		when Triangle =>
-			if sel = '00' then NS <= Sine;
-			elsif sel = '01' then NS <= Triangle;
-			elsif sel= '10' then NS <= Square;
+			if sel = "00" then NS <= Sine;
+			elsif sel = "01" then NS <= Triangle;
+			elsif sel= "10" then NS <= Square;
 			else NS <= Special;
 			end if;
-			operation <= '01';
+			operation <= "01";
 		when Square =>
-			if sel = '00' then NS <= Sine;
-			elsif sel = '01' then NS <= Triangle;
-			elsif sel= '10' then NS <= Square;
+			if sel = "00" then NS <= Sine;
+			elsif sel = "01" then NS <= Triangle;
+			elsif sel= "10" then NS <= Square;
 			else NS <= Special;
 			end if;
-			operation <= '10';
+			operation <= "10";
 		when Special =>
-			if sel = '00' then NS <= Sine;
-			elsif sel = '01' then NS <= Triangle;
-			elsif sel= '10' then NS <= Square;
+			if sel = "00" then NS <= Sine;
+			elsif sel = "01" then NS <= Triangle;
+			elsif sel= "10" then NS <= Square;
 			else NS <= Special;
 			end if;
-			operation <= '11';
+			operation <= "11";
 	end case;
 end process;
 end FSM_arch;
