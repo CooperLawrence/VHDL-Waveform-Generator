5c1a922bc05164616ae8124446adb2f0ba4afabc
Cooper Lawrence <clawr014@uottawa.ca>
2018-11-26 21:54:40 -0500

    Created datalatch component

---
+++ src/Datalatch.vhd
@@ -0,0 +1,22 @@
+import ieee;
+use ieee.std_logic_1164.all;
+
+entity datalatch is
+	port (
+		clk: in std_logic;
+		d: in std_logic_vector(31 downto 0);
+
+		q: out std_logic_vector(31 downto 0)
+	);
+end datalatch;
+
+--32-bit d flip-flop
+architecture datalatcharch is
+begin
+	process(clk)
+	begin
+		if (clk'event and clk = '1') then
+			q <= d;
+		end if;
+	end process;
+end datalatcharch;
